23b9bc4eb321b22be5dba1137576429b479c8d0f
K3ntch4na <143719469+k3ntch4n@users.noreply.github.com>
2025-04-13 16:57:01 +0200

    Update recontool.py

--- recontool.py
+++ recontool.py
@@ -58,7 +58,8 @@ save_output(f"{output_dir}/nmap_results.txt", nmap_output)
 
 # Wafw00f
 print("Exécution de Wafw00f...")
-wafw00f_output = run_command(f"wafw00f http://{target}")
+waf_url = f"http://{waf_host}"
+wafw00f_output = run_command(f"wafw00f {waf_url}")
 save_output(f"{output_dir}/wafw00f_results.txt", wafw00f_output)
 
 # CORScanner
